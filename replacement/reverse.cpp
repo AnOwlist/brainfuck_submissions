1c04141e963dbb3443a851e8c1274a3944aa8a92
AnOwlist <AnOwlist.bf@gmail.com>
2025-05-07 03:37:04 +0900

    delete unnecessary part of replacement

--- replacement/reverse.cpp
+++ replacement/reverse.cpp
@@ -1,26 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-using ld = long double;
-template <class T> using v = vector<T>;
-template <class T> using pq = priority_queue<T>;
-template <class T> using rpq = priority_queue<T, v<T>, greater<T>>;
-#define ft first
-#define sd second
-#define pb push_back
-#define all(a) a.begin(), a.end()
-#define rall(a) a.rbegin(), a.rend()
-#define rep(i, a, b) for (int i = a; i < b; i++)
-#define rrep(i, a, b) for (int i = a; i >= b; i--)
-
-template <class T> bool chmin(T &a, T b) {
-  return (a > b ? a = b, true : false);
-}
-template <class T> bool chmax(T &a, T b) {
-  return (a < b ? a = b, true : false);
-}
-
 int main() {
   map<char, char> mp = {{'<', '>'}, {'>', '<'}};
 
